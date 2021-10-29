28d97b452ed48d24985900c275f4d5bfe60d71ee
Rafa <rafael.romero@institutvidreres.cat>
2021-10-29 15:46:41 +0200

    Extracte dels moviments d’un compte (Capa Domini i Persistència)

--- src/CapaDomini/Moviment.java
+++ src/CapaDomini/Moviment.java
@@ -3,34 +3,37 @@ package CapaDomini;
 import java.util.Date;
 
 public class Moviment {
-    
+
     public int num_operacio;
     public int tipus; // 1=INGRES;
-                      //-1=REINTEGRAMENT
+    //-1=REINTEGRAMENT
     public int quantitat;
     public Date data;
-    
     public Moviment(int tipus,int quantitat){
-        
+        this.tipus = tipus;
+        this.quantitat= quantitat;
     }
-    
+
     public Moviment(int num_operacio,int tipus,int quantitat, Date data){
-        
+        this.num_operacio = num_operacio;
+        this.tipus = tipus;
+        this.quantitat = quantitat;
+        this.data = data;
     }
-    
+
     public int getQuantitat(){
-        
-        return -1;
+
+        return this.quantitat;
     }
-    
+
     public int getTipus(){
-        
-        return -1;
+
+        return this.tipus;
     }
-            
+
     public String toString(){
-        
-        return null;
+        return this.tipus == 1 ? "Número operació: " + this.num_operacio + ", Tipus: Ingrés, Quantitat: " + this.quantitat + ", Data: " + this.data : "Número operació: " + this.num_operacio + ", Tipus: Reintegrament, Quantitat: " + this.quantitat + ", Data: " + this.data;
+
     }
     
 }
